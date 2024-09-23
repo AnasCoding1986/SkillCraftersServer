688f87849fb635d080e0bb0b800aa2548e7c22bf
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-09-23 07:35:45 +0600

    single data fetch api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -37,6 +37,15 @@ async function run() {
     // Get All jobs data from db
     app.get("/jobs", async (req,res) => {
       const result = await jobsCollection.find().toArray();
+
+      res.send(result)
+    })
+
+    // Get Single jobs data from db
+    app.get("/job/:id", async (req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await jobsCollection.findOne(query);
 
       res.send(result)
     })
